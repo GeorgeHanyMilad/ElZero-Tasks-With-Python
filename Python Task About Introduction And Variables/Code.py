cc10a4d06da79c446914f9a40861062f4481457e
George Hany Milad <92086186+GeorgeHanyMilad@users.noreply.github.com>
2024-07-30 09:49:01 +0300

    Task

---
+++ Python Task About Introduction And Variables/Code.py
@@ -0,0 +1,19 @@
+'''
+Python Task
+About:
+Introduction And Variables
+'''
+
+My_Name = "George Hany Milad"
+My_Age = "21"
+My_Country = "Egypt"
+
+print(f"Name: {My_Name}")
+print(f"Age: {My_Age}")
+print(f"Country: {My_Country}", '\n')
+
+print(f"Hello, My Name Is {My_Name} And Iam {My_Age} Years Old And I Live in {My_Country}.", '\n')
+
+print(type(My_Name))
+print(type(My_Age))
+print(type(My_Country))
