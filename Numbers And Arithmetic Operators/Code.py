f437d2d808650eb297917c6bc4507da9f24d3be1
George Hany Milad <92086186+GeorgeHanyMilad@users.noreply.github.com>
2024-07-31 19:41:02 +0300

    Task

---
+++ Numbers And Arithmetic Operators/Code.py
@@ -0,0 +1,21 @@
+print(type(10)) # Integer
+print(type(10.5)) # Float
+print(type(10+6j), '\n') # Complex
+
+num = 1+2j
+print(num.imag)
+print(num.real, '\n')
+
+num = 10
+print(f"{num:.10f}", '\n')
+
+num = 159.650
+num = int(num)
+print(num)
+print(type(num), '\n')
+
+print(f"100 - 115 = {100 - 115}")
+print(f"50 * 30 = {50 * 30}")
+print(f"21 % 4 = {21 % 4}")
+print(f"110 / 11 = {110 // 11}")
+print(f"97 / 20 = {97 // 20}", '\n')
